db97ff21da6194d8179e857f712639d3f5848930
Elysium Hosack <elysium.hosack@mymail.champlain.edu>
2024-09-05 15:51:22 -0400

    Lab 1 completed.

---
+++ PowerShell Basic Lab - 1/lab1.ps1
@@ -0,0 +1,5 @@
+﻿# (Get-NetIPAddress -AddressFamily IPv4 | Where-Object { $_.InterfaceAlias -ilike "ethernet0" }).IPAddress
+# (Get-NetIPAddress -AddressFamily IPv4 | Where-Object { $_.InterfaceAlias -ilike "ethernet0" }).PrefixLength
+# Get-WmiObject -List | Where-Object { $_.Name -ilike "Win32_Net*" } | Sort-Object
+#Get-CimInstance Win32_NetworkAdapterConfiguration -Filter "DHCPEnabled=$true" | Select DHCPServer | Format-Table -HideTableHeaders
+(Get-DnsClientServerAddress -AddressFamily IPv4 | Where-Object { $_.InterfaceAlias -ilike "ethernet0" }).ServerAddresses[0]
---
+++ PowerShell Basic Lab - 1/part2.ps1
@@ -0,0 +1,29 @@
+﻿# cd '.\Documents\PowerShell Basic Lab - 1'
+#
+# $files=(Get-ChildItem)
+# for ($j=0; $j -le $files.Length; $j++) {
+#     if($files[$j].Name -ilike "*ps1") {
+#         Write-Host $files[$j].Name
+#     }
+# }
+
+#$folderpath="$PSScriptRoot\outfolder"
+#if(Test-Path $folderpath) {
+#    Write-Host "Folder Already Exists"
+#}
+#else {
+#    New-Item -ItemType Directory -Path $folderpath
+#}
+
+#cd $PSScriptRoot
+#$files=(Get-ChildItem)
+#
+#$folderpath = "$PSScriptRoot\outfolder"
+#$filePath = Join-Path $folderpath "out.csv"
+#
+#$files | Where-Object { $_.Extension -eq ".ps1" } |
+#Export-Csv -Path $filePath
+
+$files = Get-ChildItem -Recurse -File
+$files | Rename-Item -NewName { $_.Name -replace '.csv', '.log' } | Write-Host
+Get-ChildItem -Recurse -File
