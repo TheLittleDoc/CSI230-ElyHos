fe82d6c9deec17842369d2ed9fb8a34b04b71f70
Elysium Hosack <elysium.hosack@mymail.champlain.edu>
2024-09-05 16:43:38 -0400

    Process Management Lab complete

---
+++ Process Management - 1/lab1.ps1
@@ -0,0 +1,26 @@
+﻿clear
+#Get-Process | Where-Object { $_.ProcessName -ilike "c*" }
+#Get-Process | Where-Object { $_.Path -inotlike "*system32*" }
+
+#$folderpath="$PSScriptRoot\outfolder"
+#if(Test-Path $folderpath) {
+#    Write-Host "Folder Already Exists"
+#}
+#else {
+#    New-Item -ItemType Directory -Path $folderpath
+#}
+
+#cd $folderpath
+
+#$filePath = Join-Path $folderpath "out.csv"
+
+#$services = (Get-Service | Where-Object { $_.Status -ilike "Stopped" })
+#$services
+#$services | Sort-Object | Export-Csv -Path $filePath
+
+
+if ((Get-Process chrome -ea SilentlyContinue)) {
+    Get-Process chrome | Stop-Process
+} else {
+    Start-Process -FilePath "C:\Program Files\Google\Chrome\Application\chrome.exe" -ArgumentList '"https://champlain.edu"'
+}
