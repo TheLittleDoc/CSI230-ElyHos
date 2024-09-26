3561e0eab845a19d45f1a39d72a5af36b65e9c7f
Elysium Hosack <elysium.hosack@mymail.champlain.edu>
2024-09-26 16:28:18 -0400

    Completed Parsing Apache Logs assignment.

---
+++ Parsing Apache Logs/Get-ApacheLogs.ps1
@@ -0,0 +1,22 @@
+﻿function Get-ApacheLogs() {
+    $logs = Get-Content C:\xampp\apache\logs\access.log
+    $tableLog = @()
+
+    for($i = 0; $i -lt $logs.Count; $i++) {
+        $words = $logs[$i].Split(" ")
+        $tableLog += [pscustomobject]@{
+            "IP" = $words[0];
+            "Time" = $words[3].TrimStart("[") + " " + $words[4].TrimEnd("]");
+            "Method" = $words[5].TrimStart('"');
+            "Page" = $words[6];
+            "Protocol" = $words[7].TrimEnd('"');
+            "Response" = $words[8];
+            "Referrer" = $words[10];
+            "Client" = $words[11..($words.Length - 1)] -join " "
+        }
+    }
+    return $tableLog | Where-Object { $_.IP -ilike "1*" }
+}
+
+$tableOutput = Get-ApacheLogs
+$tableOutput | Format-Table -AutoSize
--- Windows Apache Logs/main.ps1
+++ Windows Apache Logs/main.ps1
@@ -1,8 +1,11 @@
-﻿cd "C:\Users\champuser\Documents\CSI230-ElyHos\Windows Apache Logs\"
-. (Join-Path $PSScriptRoot "Get-Visitors.ps1")
+﻿. (Join-Path $PSScriptRoot "Windows Apache Logs\Get-Visitors.ps1")
+. (Join-Path $PSScriptRoot "Parsing Apache Logs\Get-ApacheLogs.ps1")
 
-$ips = Get-Visitors -page "/index.html" -code "200" -browser "Firefox"
+#$ips = Get-Visitors -page "/index.html" -code "200" -browser "Firefox"
+#
+#$ipsoften = $ips | Where-Object { $_.IP -ilike "1*" }
+#$counts = $ipsoften | Group-Object IP
+#$counts | Select Count,Name
 
-$ipsoften = $ips | Where-Object { $_.IP -ilike "1*" }
-$counts = $ipsoften | Group-Object IP
-$counts | Select Count,Name
+$tableOutput = Get-ApacheLogs
+$tableOutput | Format-Table -AutoSize
